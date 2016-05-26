6d86ddc89b1ef61ba12e30c34a55d4d8bc6f1192
Louis Le <louis.le@sas.com>
2016-05-26 13:59:18 -0400

    Finished Top Bar Look

--- music/views.py
+++ music/views.py
@@ -1,13 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
 	all_albums = Album.objects.all()
-	html = ''
-	for album in all_albums:
-		url = '/music/' + str(album.id) + '/'
-		html += '<a href = "' + url + '">' + album.album_title + '</a><br>'
-	return HttpResponse(html)
+	return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-	return HttpResponse("<h2>Details for Album id: " + str(album_id + "</h2>")
+	album = get_object_or_404(Album, pk = album_id)
+	return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+	album = get_object_or_404(Album, pk = album_id)
+	try:
+		# POST is form method
+		selected_song = album.song_set.get(pk = request.POST['song'])
+	except(KeyError, Song.DoesnotExist):
+		return render(request, 'music/detail.html', {
+			'album': album,
+			'error_message': 'You did not select a valid song',
+		})
+	else:
+		selected_song.is_favorite = True
+		selected_song.save()
+		return render(request, 'music/detail.html', {'album': album})
